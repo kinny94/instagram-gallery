f90c5797d899495e46a5c0998b9c24afdfe73a11
kinny94 <ad.sam007@gmail.com>
2017-11-15 21:12:43 -0500

    Added controllers

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
-var app = angular.module('galPhotos', ['ng-route']);
+var app = angular.module('galPhoto', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider.
         when('/gallery', {
             templateUrl: 'views/gallery.view.html',
             controller: 'GalleryCtrl'
-        }).
-        otherwise({redirectTo: '/gallery'});
+        })
+        .otherwise({redirectTo: '/gallery'});
 }]);
